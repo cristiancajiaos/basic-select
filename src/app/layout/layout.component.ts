4973eeb36c65af70c9a892172b9de41bc135dc76
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-06-06 11:52:10 -0400

    Creado elemento select con double binding

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
+  foo: any[] = [
+    {name: 'One', value: 1},
+    {name: 'Two', value: 2},
+    {name: 'Three', value: 3}
+  ];
+
+  bar: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.bar = this.foo[0].value;
   }
 
 }
